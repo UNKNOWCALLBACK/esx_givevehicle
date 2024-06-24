8ab03394733028a737a108885bb12525b19562fd
UNKNOWCALLBACK_TEAM <147795365+UNKNOWCALLBACK@users.noreply.github.com>
2024-06-24 09:04:49 +0700

    Update config.function.lua

--- config.function.lua
+++ config.function.lua
@@ -1,1 +1,4 @@
-
+config = {}
+config.debugtext = function (playername,_source,type,plate)
+print('[ESX_GIVEVEHICLE] : PLAYER' ..playername.. 'ID' .._source.. 'RECEIVE' ..type.. 'PLATE' ..plate )
+end
